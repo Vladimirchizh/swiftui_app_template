ea2dbd048b9105d6a397ec30210350dada351a95
Vladimir <vdchizhevskiy@gmail.com>
2021-12-13 15:42:40 +0300

    Initial Commit

---
+++ swiftui_app_template/swiftui_app_templateApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_app_templateApp.swift
+//  swiftui_app_template
+//
+//  Created by Apple on 13.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_app_templateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
