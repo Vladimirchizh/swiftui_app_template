144ad7823864ee92b35759686e57bbe7b57220b4
Vladimir <vdchizhevskiy@gmail.com>
2021-12-14 00:12:02 +0300

    adding some stacks and animations

--- swiftui_app_template/ContentView.swift
+++ swiftui_app_template/ContentView.swift
@@ -7,25 +7,112 @@
 
 import SwiftUI
 
+class Colors {
+  let colorTop = UIColor(red: 192.0/255.0, green: 38.0/255.0, blue: 42.0/255.0, alpha: 1.0)
+  let colorBottom = UIColor(red: 35.0/255.0, green: 2.0/255.0, blue: 2.0/255.0, alpha: 1.0)
+
+  let gl: CAGradientLayer
+
+  init() {
+    gl = CAGradientLayer()
+    gl.colors = [ colorTop, colorBottom]
+    gl.locations = [ 0.0, 1.0]
+  }
+}
+
 struct ContentView: View {
     @Namespace var namespace
     @State var show = false
+    let colors = Colors()
+
     var body: some View {
-        ZStack {
-            if !show {
-                Text("StyleGAN")
-                    .padding().frame(width: .infinity,
-                                     alignment: .leading)
-            } else {
-                Text("StyleGAN")
-                    .padding().frame(width: .infinity,
-                                     alignment: .trailing)
-            }
-        }.onTapGesture {
-            withAnimation{
-                show.toggle()
-                
-            }
+            VStack{ZStack {
+                if !show {
+                    VStack(alignment:.leading, spacing: 12){
+                        Text("StyleGAN")
+                            .font(.largeTitle.weight(.black))
+                            .matchedGeometryEffect(id: "title", in: namespace)
+                            .frame(maxWidth: .infinity,
+                                   alignment: .leading)
+                        Text("Image generation with StyleGAN and CLIP".uppercased())
+                            .font(.footnote.weight(.semibold))
+                            .matchedGeometryEffect(id: "subtitle", in: namespace)
+                        
+                    }
+                    .padding(20)
+                    .foregroundColor(.white)
+                    .background(Color.green.matchedGeometryEffect(id: "background", in: namespace))
+                    .padding(20)
+                } else {
+                    
+                    VStack(alignment:.leading, spacing: 12){
+                        Spacer()
+                        Text("Image generation with StyleGAN and CLIP".uppercased())
+                            .font(.footnote.weight(.semibold))
+                            .matchedGeometryEffect(id: "subtitle", in: namespace)
+                        Text("StyleGAN")
+                            .font(.largeTitle.weight(.black))
+                            .matchedGeometryEffect(id: "title", in: namespace)
+                            .frame(maxWidth: .infinity,
+                                   alignment: .leading)
+                        
+                        
+                    }
+                    .padding(20)
+                    .foregroundColor(.red)
+                    .background(Color.black.matchedGeometryEffect(id: "background", in: namespace))
+                }
+            
+            }.onTapGesture {
+                withAnimation{
+                    show.toggle()
+                    
+                }
+            }.offset(x:0,y:15)
+            
+            
+            ZStack {
+                if !show {
+                    VStack(alignment:.leading, spacing: 12){
+                        Text("StyleGAN")
+                            .font(.largeTitle.weight(.black))
+                            .matchedGeometryEffect(id: "title", in: namespace)
+                            .frame(maxWidth: .infinity,
+                                   alignment: .leading)
+                        Text("Image generation with StyleGAN and CLIP".uppercased())
+                            .font(.footnote.weight(.semibold))
+                            .matchedGeometryEffect(id: "subtitle", in: namespace)
+                        
+                    }
+                    .padding(20)
+                    .foregroundColor(.white)
+                    .background(Color.green.matchedGeometryEffect(id: "background", in: namespace))
+                    .padding(20)
+                } else {
+                    
+                    VStack(alignment:.leading, spacing: 12){
+                        Spacer()
+                        Text("Image generation with StyleGAN and CLIP".uppercased())
+                            .font(.footnote.weight(.semibold))
+                            .matchedGeometryEffect(id: "subtitle", in: namespace)
+                        Text("StyleGAN")
+                            .font(.largeTitle.weight(.black))
+                            .matchedGeometryEffect(id: "title", in: namespace)
+                            .frame(maxWidth: .infinity,
+                                   alignment: .leading)
+                        
+                        
+                    }
+                    .padding(20)
+                    .foregroundColor(.red)
+                    .background(Color.black.matchedGeometryEffect(id: "background", in: namespace))
+                }
+            }.onTapGesture {
+                withAnimation{
+                    show.toggle()
+                    
+                }
+            }.offset(x:0,y:15)
         }
     }
 }
@@ -33,5 +120,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 12")
     }
 }
