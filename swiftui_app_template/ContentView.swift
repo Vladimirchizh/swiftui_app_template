870ab9cd6107b12507af5fe00fa45e852f2278be
Vladimir <vdchizhevskiy@gmail.com>
2021-12-21 15:56:17 +0300

    adding custom image, changing stacks

--- swiftui_app_template/ContentView.swift
+++ swiftui_app_template/ContentView.swift
@@ -7,26 +7,12 @@
 
 import SwiftUI
 
-class Colors {
-  let colorTop = UIColor(red: 192.0/255.0, green: 38.0/255.0, blue: 42.0/255.0, alpha: 1.0)
-  let colorBottom = UIColor(red: 35.0/255.0, green: 2.0/255.0, blue: 2.0/255.0, alpha: 1.0)
-
-  let gl: CAGradientLayer
-
-  init() {
-    gl = CAGradientLayer()
-    gl.colors = [ colorTop, colorBottom]
-    gl.locations = [ 0.0, 1.0]
-  }
-}
-
 struct ContentView: View {
     @Namespace var namespace
     @State var show = false
-    let colors = Colors()
 
     var body: some View {
-            VStack{ZStack {
+        ZStack {
                 if !show {
                     VStack(alignment:.leading, spacing: 12){
                         Text("StyleGAN")
@@ -41,7 +27,11 @@ struct ContentView: View {
                     }
                     .padding(20)
                     .foregroundColor(.white)
-                    .background(Color.green.matchedGeometryEffect(id: "background", in: namespace))
+                    .background(Image("pro1_out")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .matchedGeometryEffect(id: "background", in: namespace)
+                    )
                     .padding(20)
                 } else {
                     
@@ -59,61 +49,20 @@ struct ContentView: View {
                         
                     }
                     .padding(20)
-                    .foregroundColor(.red)
-                    .background(Color.black.matchedGeometryEffect(id: "background", in: namespace))
-                }
-            
-            }.onTapGesture {
-                withAnimation{
-                    show.toggle()
-                    
-                }
-            }.offset(x:0,y:15)
-            
-            
-            ZStack {
-                if !show {
-                    VStack(alignment:.leading, spacing: 12){
-                        Text("StyleGAN")
-                            .font(.largeTitle.weight(.black))
-                            .matchedGeometryEffect(id: "title", in: namespace)
-                            .frame(maxWidth: .infinity,
-                                   alignment: .leading)
-                        Text("Image generation with StyleGAN and CLIP".uppercased())
-                            .font(.footnote.weight(.semibold))
-                            .matchedGeometryEffect(id: "subtitle", in: namespace)
-                        
-                    }
-                    .padding(20)
                     .foregroundColor(.white)
-                    .background(Color.green.matchedGeometryEffect(id: "background", in: namespace))
-                    .padding(20)
-                } else {
-                    
-                    VStack(alignment:.leading, spacing: 12){
-                        Spacer()
-                        Text("Image generation with StyleGAN and CLIP".uppercased())
-                            .font(.footnote.weight(.semibold))
-                            .matchedGeometryEffect(id: "subtitle", in: namespace)
-                        Text("StyleGAN")
-                            .font(.largeTitle.weight(.black))
-                            .matchedGeometryEffect(id: "title", in: namespace)
-                            .frame(maxWidth: .infinity,
-                                   alignment: .leading)
-                        
-                        
-                    }
-                    .padding(20)
-                    .foregroundColor(.red)
-                    .background(Color.black.matchedGeometryEffect(id: "background", in: namespace))
+                    .background(Image("pro1_out")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .matchedGeometryEffect(id: "background", in: namespace)
+                                )
                 }
+            
             }.onTapGesture {
                 withAnimation{
                     show.toggle()
                     
                 }
-            }.offset(x:0,y:15)
-        }
+            }
     }
 }
 
