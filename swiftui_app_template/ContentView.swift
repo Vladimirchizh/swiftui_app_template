28b6e000e1ec3949b5e0c684fd3d940492c65e9b
Владимир Чижевский <vdc@Vladimirs-MBP.lan>
2021-12-27 17:50:31 +0300

    changing the look in swiftui

--- swiftui_app_template/ContentView.swift
+++ swiftui_app_template/ContentView.swift
@@ -14,24 +14,39 @@ struct ContentView: View {
     var body: some View {
         ZStack {
                 if !show {
-                    VStack(alignment:.leading, spacing: 12){
-                        Text("StyleGAN")
-                            .font(.largeTitle.weight(.black))
-                            .matchedGeometryEffect(id: "title", in: namespace)
-                            .frame(maxWidth: .infinity,
+                    VStack{
+                        Spacer()
+                        VStack(alignment:.leading, spacing: 12) {
+                            Text("StyleGAN")
+                                .font(.largeTitle.weight(.black))
+                                .matchedGeometryEffect(id: "title", in: namespace)
+                                .frame(maxWidth: .infinity,
                                    alignment: .leading)
-                        Text("Image generation with StyleGAN and CLIP".uppercased())
-                            .font(.footnote.weight(.semibold))
-                            .matchedGeometryEffect(id: "subtitle", in: namespace)
+                            Text("Image generation with StyleGAN and CLIP".uppercased())
+                                .font(.footnote.weight(.semibold))
+                                .matchedGeometryEffect(id: "subtitle", in: namespace)
+                        }
+                        .padding(20)
+                        .background(
+                            Rectangle()
+                                .fill(.ultraThinMaterial)
+                                .mask(RoundedRectangle(cornerRadius: 30,style: .continuous))
+                                .blur(radius: 30)
+                                .matchedGeometryEffect(id: "blur", in: namespace)
+                        )
                         
                     }
-                    .padding(20)
                     .foregroundColor(.white)
-                    .background(Image("pro1_out")
+                    .background(Image("Group 15")
                                     .resizable()
-                                    .aspectRatio(contentMode: .fill)
+                                    .aspectRatio(contentMode: .fit )
+                                    .matchedGeometryEffect(id: "image", in: namespace)
+                                )
+                    .background(Image("Rectangle 8")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill )
                                     .matchedGeometryEffect(id: "background", in: namespace)
-                    )
+                                )
                     .mask(RoundedRectangle(cornerRadius: 30, style: .continuous)
                             .matchedGeometryEffect(id: "mask", in: namespace)
                             )
@@ -40,29 +55,56 @@ struct ContentView: View {
                 } else {
                     
                     ScrollView {
-                        VStack(alignment:.leading, spacing: 12){
+                        VStack{
                             Spacer()
-                            Text("Image generation with StyleGAN and CLIP".uppercased())
-                                .font(.footnote.weight(.semibold))
-                                .matchedGeometryEffect(id: "subtitle", in: namespace)
-                            Text("StyleGAN")
-                                .font(.largeTitle.weight(.black))
-                                .matchedGeometryEffect(id: "title", in: namespace)
-                                .frame(maxWidth: .infinity,
-                                       alignment: .leading)
-                            
-                            
                         }
+                        .frame(maxWidth: .infinity)
                         .frame(height:500)
-                        .padding(20)
-                        .foregroundColor(.white)
-                        .background(Image("pro1_out")
+                        .foregroundColor(.black)
+                        .background(Image("Group 15")
                                         .resizable()
-                                        .aspectRatio(contentMode: .fill)
+                                        .aspectRatio(contentMode: .fill )
+                                        .matchedGeometryEffect(id: "image", in: namespace)
+                                    )
+                        .background(Image("Rectangle 8")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fill )
                                         .matchedGeometryEffect(id: "background", in: namespace)
                                     )
                         .mask(RoundedRectangle(cornerRadius: 30, style: .continuous)
                                 .matchedGeometryEffect(id: "mask", in: namespace)
+                        )
+                        .overlay(
+                            VStack(alignment:.leading, spacing: 12){
+                                Text("Image generation with StyleGAN and CLIP".uppercased())
+                                    .font(.footnote.weight(.semibold))
+                                    .matchedGeometryEffect(id: "subtitle", in: namespace)
+                                Text("StyleGAN")
+                                    .font(.largeTitle.weight(.black))
+                                    .matchedGeometryEffect(id: "title", in: namespace)
+                                    .frame(maxWidth: .infinity,
+                                           alignment: .leading)
+                                Divider()
+                                HStack{
+                                    Image("Avatar Default")
+                                        .resizable()
+                                        .frame(width: 26, height: 26)
+                                        .cornerRadius(10)
+                                    
+                                    
+                                }
+                                
+                            }
+                                .padding(20)
+                                .background(
+                                        Rectangle()
+                                            .fill(.ultraThinMaterial)
+                                            .mask(RoundedRectangle(cornerRadius: 30,style: .continuous))
+                                            .matchedGeometryEffect(id: "blur", in: namespace)
+                                    )
+                                .offset(y:250)
+                                .padding(20)
+                            
                         )
                         
                     }
